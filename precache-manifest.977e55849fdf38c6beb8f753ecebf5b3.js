d744d7404535fb591b47c455a6bf5524d55d53a9
mahdislimane <slimane_mahdi@yahoo.fr>
2020-06-14 17:19:16 +0100

    Updates

---
+++ precache-manifest.977e55849fdf38c6beb8f753ecebf5b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edef0c640e2681fd88d4d9aeff54fd7f",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "1513617431b4e36e87cb",
+    "url": "/mycv/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bdf21c728294561c331a",
+    "url": "/mycv/static/css/main.0d39530c.chunk.css"
+  },
+  {
+    "revision": "1513617431b4e36e87cb",
+    "url": "/mycv/static/js/2.d8d622c7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mycv/static/js/2.d8d622c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf21c728294561c331a",
+    "url": "/mycv/static/js/main.fa2c9da9.chunk.js"
+  },
+  {
+    "revision": "11c2afd8d99341db2ed8",
+    "url": "/mycv/static/js/runtime-main.14fd0c85.js"
+  }
+]);
